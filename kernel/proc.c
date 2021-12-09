707ecf18cdeb84885e209c2a7dfa24332f90c4d5
Dashuai.Quan <Dashuai.Quan@outlook.com>
2021-12-09 12:31:06 +0800

    update

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,8 +132,9 @@ found:
     freeproc(p);
     release(&p->lock);
   }
+  #ifdef LAB_PGTBL
   p->usyscall->pid = p->pid;
-
+  #endif
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -206,14 +207,14 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
-
+  #ifdef LAB_PGTBL
   if (mappages(pagetable, USYSCALL, PGSIZE, 
                (uint64)(p->usyscall), PTE_R | PTE_V | PTE_U) < 0) {
     uvmunmap(pagetable, USYSCALL, 1, 0);
     uvmfree(pagetable, 0);
     return 0;             
   }
-
+  #endif
   return pagetable;
 }
 
@@ -224,7 +225,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  #ifdef LAB_PGTBL
   uvmunmap(pagetable, USYSCALL, 1, 0);
+  #endif
   uvmfree(pagetable, sz);
 }
 
